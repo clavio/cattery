66f24582cef1c88ecac17cd69541431617f75269
Joseph Barra <jbarra@datto.com>
2020-08-20 14:45:01 -0400

    initial commit

---
+++ app/src/main/java/com/ciphra/android/thecattery/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ciphra.android.thecattery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ciphra.android.thecattery.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ciphra/android/thecattery/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ciphra.android.thecattery.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
